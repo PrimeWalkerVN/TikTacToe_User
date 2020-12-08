70fb655bc26e49cc16bedf2d1c8a4e0302c12b3f
PrimeWalkerVN <primewalker@PrimeWalkers-MacBook-Pro.local>
2020-12-08 22:10:53 +0700

    fix authentication test socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, Route, Router, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import NotFound from './components/common/Notfound';
@@ -10,10 +10,37 @@ import Loading from './components/common/Loading';
 import PrivateRouteAuth from './components/auth/PrivateRouteAuth';
 import PrivateRoute from './components/auth/PrivateRoute';
 import { RootState } from './types/Reducer';
+import { setIsLoading } from './redux/reducers/loadingReducer';
+import usersApi from './api/userApi';
+import Notification from './components/common/Notification';
+import { setLogged, setUser } from './redux/reducers/userReducer';
 
 const history = createBrowserHistory();
 const App: React.FC = () => {
   const isLoading = useSelector((state: RootState) => state.loading.isLoading);
+
+  const token = localStorage.getItem('access_token');
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setIsLoading(true));
+    if (token) {
+      const callMe = async () => {
+        try {
+          const user = await usersApi.getMe();
+          setIsLoading(true);
+          if (user) {
+            dispatch(setUser(user));
+            dispatch(setLogged(true));
+          }
+        } catch (e) {
+          Notification('error', 'Error', e.message);
+        }
+      };
+      callMe();
+    }
+    dispatch(setIsLoading(false));
+  }, [token, dispatch]);
+
   return (
     <div className="container max-w-full">
       {isLoading ? (
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,6 +1,8 @@
+import { Button } from 'antd';
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { io } from 'socket.io-client';
 import { logout } from '../../redux/reducers/userReducer';
 import { RootState } from '../../types/Reducer';
 import Header from '../common/Header';
@@ -8,6 +10,7 @@ import UsersStatus from './UserStatus/UsersStatus';
 
 const Dashboard: React.FC = () => {
   const dispatch = useDispatch();
+  const ENDPOINT = 'http://localhost:8080';
 
   const user: any = useSelector((state: RootState) => state.user.user);
   const history = useHistory();
@@ -20,6 +23,12 @@ const Dashboard: React.FC = () => {
   const redirectHomeHandler = () => {
     history.push('/dashboard');
   };
+
+  const emitFunction = () => {
+    const socket = io(ENDPOINT);
+    socket.emit('message');
+  };
+
   return (
     <div className="w-full">
       <Header
@@ -31,6 +40,7 @@ const Dashboard: React.FC = () => {
       />
       <div className="flex justify-center w-full">
         <UsersStatus />
+        <Button onClick={() => emitFunction()}>Emmit</Button>
       </div>
     </div>
   );
